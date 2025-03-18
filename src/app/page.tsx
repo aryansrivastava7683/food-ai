921994ecac9539529bfd82835d534f2eef2dbfeb
Night <96472381+aryansrivastava7683@users.noreply.github.com>
2025-03-18 15:11:22 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,19 +22,7 @@ export default function Home() {
     });
   }, []);
 
-  const handleDownload = () => {
-    const apkUrl = "/app-release.apk"; // File path inside the public folder
-    const link = document.createElement("a");
-    link.href = apkUrl;
-    link.download = "myapp.apk"; // Forces download
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-    } else {
-      alert("Install not available");
-    }
-  };
+  
 
   return (
     <div className="flex flex-col justify-center items-center p-8 min-h-screen">
@@ -49,12 +37,6 @@ export default function Home() {
         />
         <h1 className="font-bold text-2xl">Welcome to Food Track</h1>
         <div className="flex gap-4">
-          <button
-            onClick={handleDownload}
-            className="bg-blue-500 hover:bg-blue-700 px-4 py-2 rounded text-white"
-          >
-            Install App
-          </button>
           <button
             onClick={() => router.push("/login")}
             className="bg-green-500 hover:bg-green-700 px-4 py-2 rounded text-white"
